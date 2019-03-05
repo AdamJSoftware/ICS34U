293a7dfaa0f8115e675dae6c163ff850e133a5f8
ViteloSoftware <42972871+ViteloSoftware@users.noreply.github.com>
2019-03-05 14:19:40 -0500

    Practice projects

---
+++ src/JAVA.java
@@ -0,0 +1,8 @@
+public class JAVA {
+    public static void main(String[] args){
+        System.out.println("\tJ\t A  V     V  A");
+        System.out.println("\tJ   AAA  V   V  AAA");
+        System.out.println("J\tJ  AAAAA  V V  AAAAA");
+        System.out.println(" J J  A     A  V  A     A");
+    }
+}
