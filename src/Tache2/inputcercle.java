5319fa692cc855287b1990cbf023fb455fcd1870
ViteloSoftware <42972871+ViteloSoftware@users.noreply.github.com>
2019-03-18 13:43:06 -0400

    Practice projects

--- src/Tache2/inputcercle.java
+++ src/Tache2/inputcercle.java
@@ -5,12 +5,22 @@ import java.util.Scanner;
 public class inputcercle {
     public static void main(String[]args){
         final double pi = 3.14159;
+        boolean j = false;
         Scanner input = new Scanner(System.in);
-
         System.out.println("Mesure du rayon: ");
-        double rayon = input.nextDouble();
-        double aire = Math.pow(rayon,2) * pi;
-        System.out.println("L'aire du cecrcle avec le rayon de " + rayon + " est " + aire);
+        while (!j){
+            if (input.hasNextDouble()){
+                double rayon = input.nextDouble();
+                double aire = Math.pow(rayon,2) * pi;
+                System.out.println("L'aire du cecrcle avec le rayon de " + rayon + " est " + aire);
+                j = true;
+            }
+            else{
+                System.out.println("L'input n'est pas un chiffre");
+                input.next();
+            }
+        }
+
 
     }
 }
