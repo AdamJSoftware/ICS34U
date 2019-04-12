c7e56a7b0ca4b73100f37b29a5694a0d799a578d
ViteloSoftware <42972871+ViteloSoftware@users.noreply.github.com>
2019-04-12 12:21:10 -0400

    Practice projects

---
+++ src/Tache5/Travaux2.java
@@ -0,0 +1,37 @@
+package Tache5;
+
+import java.util.Scanner;
+
+public class Travaux2 {
+    public static void main(String[] args){
+
+        int nb1 = (int) (Math.random()*10);
+        int nb2 = (int) (Math.random()*10);
+
+        int total = nb1-nb2;
+
+        System.out.printf("C'est quoi le résultat de %s - %s: ", nb1, nb2);
+        double userInput = get();
+
+        if (userInput==total){
+            System.out.println("La réponse est correcte!");
+        }
+        else{
+            System.out.printf("\nMauavis réponse. La réponse est %s", total);
+        }
+    }
+
+
+    private static double get() {
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            if (input.hasNextDouble()) {
+                return input.nextDouble();
+            } else {
+                System.out.print("Insérer un nombre valide: ");
+                input.next();
+            }
+        }
+    }
+}
---
+++ src/Tache5/Travaux3.java
@@ -0,0 +1,16 @@
+package Tache5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Travaux3 {
+    public static void main(String[] args){
+        String month;
+        int num_month = (int) (1 + Math.random()*13);
+
+        var month_list = List.of("Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre");
+
+
+        System.out.printf("%s correspond à %s", num_month, month_list.get(num_month-1));
+    }
+}
