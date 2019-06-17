8ffe73b786e5199f237a4cbccf447f55958c2a10
ViteloSoftware <42972871+ViteloSoftware@users.noreply.github.com>
2019-06-17 12:54:59 -0400

    Practice projects

--- src/Exam/Travaux1.java
+++ src/Exam/Travaux1.java
@@ -1,7 +1,88 @@
+/*
+Adam Jasniewicz                                                                                         le 3 juin 2019
+
+                                                Tâche Sommative Final
+                                  But: Ce programme lit les information d'un employé
+                                  (nom, nombres d'heures salaires,
+
+ */
 package Exam;
 
-public class Travaux1 {
-    public static void main(String[] args){
+import java.util.Scanner;
+import java.text.DecimalFormat;
 
+public class Travaux1 {
+    public static void main(String[] args) {
+        // Création d'un object Decimal Format appelé df. Ceci aide pour arrondir le montant décimal
+        DecimalFormat df = new DecimalFormat("##.00");
+
+        // Demandez l'utilisateur pour les données
+        System.out.print("Entre le nom de l'employé: ");
+        String nom = getString();
+        System.out.print("Entre le nombre d'heures travaillés pour la semaine: ");
+        double heures = get();
+        System.out.print("Salaire par heure: ");
+        double salaire = get();
+        System.out.print("Taxes fédérales (en décimale): ");
+        double taxeF = get();
+        System.out.print("Taxes provinciales (en décimale): ");
+        double taxeP = get();
+        System.out.print("Retenues sur le salaire (en décimale): ");
+        double retenues = get();
+        // Calculez le salaire brut
+        double salaireBrut = heures*salaire;
+        // Affichez les résultats à l'utilisateur
+        System.out.println();
+        System.out.printf("Employé: %s\n",nom);
+        System.out.printf("Heures travaillés pour la semaine: %s\n", df.format(heures));
+        System.out.printf("Salaire par heure: %s\n", df.format(salaire));
+        System.out.printf("Salaire brut %s\n", df.format(salaireBrut));
+        System.out.println("Déductions: ");
+        System.out.printf("Taxes fédérales (%s%%): %s\n", taxeF, df.format(salaireBrut*taxeF));
+        System.out.printf("Taxes provinciales (%s%%): %s\n", taxeP, df.format(salaireBrut*taxeP));
+        System.out.printf("Retenues (%s%%): %s\n", retenues, df.format(salaireBrut*retenues));
+        System.out.printf("Salaire net: %s\n", df.format(total(salaireBrut, taxeF, taxeP, retenues)));
+
+    }
+
+
+    private static double get() {
+        // Créationz d'une fonction appelée get qui retourne un double
+        Scanner input = new Scanner(System.in);
+        // Création d'un object Scanner appelé input. Ceci prends l'entrée de l'utilisateur et le stocke temporairement dans une variable
+
+        while (true){
+            // Une boucle qui demande constamment à l'utilisateur une entrée jusqu'à ce qu'un double soit entré
+            if (input.hasNextDouble()){
+                // Si l'entrée utilisateur est un double. Faire ce qui suit
+                return input.nextDouble();
+                // Retourne le double saisi par l'utilisateur. Le retour met également fin à la fonction entière
+            }
+            else{
+                // Si l'entrée utilisateur n'est pas un double. Faire ce qui suit
+                System.out.println("Insérer un nombre valide... ");
+                // Imprime une déclaration qui dit à l'utilisateur quoi d'entrer
+                input.next();
+                // Demandez à l'utilisateur pour une autre entrée
+            }
+        }
+    }
+
+    private static String getString() {
+        // Recevoir un string de l'utilisateur (nom)
+        Scanner input = new Scanner(System.in);
+        return input.nextLine();
+    }
+
+    private static double total(double salaireBrut, double taxeF, double taxeP, double retenues){
+        // Calculation du salaire net
+        taxeF = salaireBrut*taxeF;
+        // Calculez le taxe fédéral imposer
+        taxeP = salaireBrut*taxeP;
+        // Calculez le taxe provincial imposer
+        retenues = salaireBrut*retenues;
+        // Calculez les retenues imposer
+        return salaireBrut-taxeF-taxeP-retenues;
+        // Soustraire tout du salaire brut et retournez la réponse
     }
 }
---
+++ src/Exam/Travaux2.java
@@ -0,0 +1,114 @@
+package Exam;
+
+import java.util.Scanner;
+
+public class Travaux2 {
+    public static void main(String[] args){
+        // Imprimez le menu
+        System.out.println("1. Calcule l'aire d'un cercle.");
+        System.out.println("2. Calcule l'aire d'un rectangle.");
+        System.out.println("3. Calcule l'aire d'un triangle.");
+        System.out.println("4. Quitter.");
+
+        while (true){
+            // Création d'une loop qui ne termine jamais (sauf quand l'utilisateur quitte le programme (4))
+            System.out.print(" -> ");
+            // Demandez l'utilisateur pour son selection
+            int option = getSelection();
+            // Recevoir la valeur du selection
+            switch (option) {
+                // Création d'une loop switch
+                case (4):
+                    // Si l'utilisateur entre 4
+                    System.exit(0);
+                    // Quitter le programme
+                    break;
+                case (3):
+                    // Si l'utilisateur entre 3. Demandez pour la base et l'hateur et calculé l'aire d'un triangle
+                    System.out.print("Entre la base du triangle: ");
+                    double base = get();
+                    System.out.print("Entre la hauteur du triangle: ");
+                    double hauteur = get();
+                    System.out.printf("L'aire du rectangle est égale à %s\n", (base * hauteur) / 2);
+                    // Résultat de l'aire est montrez le résultat à l'utilisateur
+                    break;
+                case (2):
+                    // Si l'utilisateur entre 2. Demandez pour la longeur et largeur et calculé l'aire d'un rectangle
+                    System.out.print("Entre la longeur du rectangle: ");
+                    double longeur = get();
+                    System.out.print("Entre la largeur du rectangle: ");
+                    double largeur = get();
+                    System.out.printf("L'aire du rectangle est égale à %s\n", longeur * largeur);
+                    // Résultat de l'aire est montrez le résultat à l'utilisateur
+                    break;
+                case (1):
+                    // Si l'utilisateur entre 1. Demandez pour le rayon et calculé l'aire d'un cercle
+                    System.out.print("Entre le rayon du cercle: ");
+                    double rayon = get();
+                    System.out.printf("L'aire est égale à %s\n", Math.pow(rayon, 2) * Math.PI);
+                    // Résultat de l'aire est montrez le résultat à l'utilisateur
+                    break;
+            }
+        }
+    }
+    private static int getSelection() {
+        // Créationz d'une fonction appelée getSelection qui retourne un double
+        Scanner input = new Scanner(System.in);
+        // Création d'un object Scanner appelé input. Ceci prends l'entrée de l'utilisateur et le stocke temporairement dans une variable
+        String in = input.next();
+        // Créer un valeur String appelé in qui prend la valeur entrée par l'utilisateur
+        while (true) {
+            // Une boucle qui demande constamment à l'utilisateur une entrée jusqu'à ce qu'un double soit entré
+            try {
+                int newIn = Integer.parseInt(in);
+                // Essayer de convertir l'entrée de l'utilisateur dans un double
+
+                if (newIn >= 1 && newIn <= 4) {
+                    // Ce instruction if va fair le suivant si le discriminant est entre 1 et 4
+
+                    return newIn;
+                    // Retourner la valeur integer newIn
+
+                }
+                else if (newIn == 5){
+                    // Ce instruction if va fair le suivant si le discriminant est supérieur a 5
+                    System.exit(0);
+                    // Le programme quitte
+                }else {
+                    // Ce instruction if va fair le suivant si le discriminant est supérieur a 4
+                    System.out.print("Insérer un nombre entre 1 et 5: ");
+                    // Imprime une déclaration qui dit à l'utilisateur quoi d'entrer
+                    in = input.next();
+                    // Créer un valeur String appelé in qui prend la valeur entrée par l'utilisateur
+                }
+            } catch (NumberFormatException | NullPointerException nfe) {
+                // Si la valeur string ne peut pas être converti en double
+                System.out.print("Insérer un nombre valide: ");
+                // Imprime une déclaration qui dit à l'utilisateur quoi d'entrer
+                in = input.next();
+                // Créer un valeur String appelé in qui prend la valeur entrée par l'utilisateur
+            }
+        }
+    }
+    private static double get() {
+        // Créationz d'une fonction appelée get qui retourne un double
+        Scanner input = new Scanner(System.in);
+        // Création d'un object Scanner appelé input. Ceci prends l'entrée de l'utilisateur et le stocke temporairement dans une variable
+
+        while (true){
+            // Une boucle qui demande constamment à l'utilisateur une entrée jusqu'à ce qu'un double soit entré
+            if (input.hasNextDouble()){
+                // Si l'entrée utilisateur est un double. Faire ce qui suit
+                return input.nextDouble();
+                // Retourne le double saisi par l'utilisateur. Le retour met également fin à la fonction entière
+            }
+            else{
+                // Si l'entrée utilisateur n'est pas un double. Faire ce qui suit
+                System.out.println("Insérer un nombre valide... ");
+                // Imprime une déclaration qui dit à l'utilisateur quoi d'entrer
+                input.next();
+                // Demandez à l'utilisateur pour une autre entrée
+            }
+        }
+    }
+}
