2e162d741e262a3d774e00a2a96f49be264a2aa1
ViteloSoftware <42972871+ViteloSoftware@users.noreply.github.com>
2019-06-19 12:13:57 -0400

    Practice projects

--- src/Exam/Travaux3.java
+++ src/Exam/Travaux3.java
@@ -1,3 +1,13 @@
+/*
+Adam Jasniewicz                                                                                         le 17 juin 2019
+
+                                               Tâche Sommative Final - 3
+                                  But: Le programme demande à l'utilisateur pour le
+                                  montant invest, le taux d'intérêt annuel et le nombre
+                                  d'années. Par la suite, les résultats sont affichées
+                                  (année, valeur finale et intérêts)
+
+ */
 package Exam;
 
 import java.text.DecimalFormat;
@@ -5,30 +15,46 @@ import java.util.Scanner;
 
 public class Travaux3 {
     public static void main(String[] args){
+        // Création d'un object Decimal Format appelé df. Ceci aide pour arrondir le montant décimal
         DecimalFormat df = new DecimalFormat("##.00");
+        //Demandez à l'utilisateur d'entrer l'information requis. L'information est pris par la fonciton get
         System.out.print("Entrer le montant investi: ");
         double montant = get();
         System.out.print("Entrer le taux d'intérêt annuel (en décimale): ");
         double taux = get();
         System.out.print("Entrer le nombre d'années: ");
         double temps = get();
+        // Les colones du tableau sont créé
         System.out.println("Année\t\tValeur finale\t\tIntérêts");
-
+        // Une loop for est crée pour imprimer les valeurs de chaque année
         for (int i = 1; i <= temps; i++){
+            // Le valeur final est calculé avec l'équation (C(1+r)^n
             double vf = montant*Math.pow((1+taux),i);
+            // L'intérêt est calculé
             double interet = vf-montant;
+            // L'information est présenté à l'utilisateur
             System.out.printf("%s\t\t\t%s\t\t\t%s\n", i, df.format(vf), df.format(interet));
         }
 
     }
     private static double get() {
+        // Créationz d'une fonction appelée get qui retourne un double
         Scanner input = new Scanner(System.in);
-        while (true) {
-            if (input.hasNextDouble()) {
+        // Création d'un object Scanner appelé input. Ceci prends l'entrée de l'utilisateur et le stocke temporairement dans une variable
+
+        while (true){
+            // Une boucle qui demande constamment à l'utilisateur une entrée jusqu'à ce qu'un double soit entré
+            if (input.hasNextDouble()){
+                // Si l'entrée utilisateur est un double. Faire ce qui suit
                 return input.nextDouble();
-            } else {
-                System.out.print("Insérer un nombre valide: ");
+                // Retourne le double saisi par l'utilisateur. Le retour met également fin à la fonction entière
+            }
+            else{
+                // Si l'entrée utilisateur n'est pas un double. Faire ce qui suit
+                System.out.println("Insérer un nombre valide... ");
+                // Imprime une déclaration qui dit à l'utilisateur quoi d'entrer
                 input.next();
+                // Demandez à l'utilisateur pour une autre entrée
             }
         }
     }
--- src/Exam/Travaux5.java
+++ src/Exam/Travaux5.java
@@ -1,14 +1,27 @@
+/*
+Adam Jasniewicz                                                                                         le 17 juin 2019
+
+                                               Tâche Sommative Final - 5
+                                  But: Le programme demande à l'utilisateur d'entrer
+                                  une lettre et ensuite affiche le chiffre correspondant
+                                  à la lettre tapée. Si l'utilisateur entre 0, le programme
+                                  quitte.
+
+ */
 package Exam;
 
 import java.util.Scanner;
 
 public class Travaux5 {
     public static void main(String[] args){
+        // Cette boucle termine seulement lorsque le fonction get retourne une valeur nulle (lorsque l'utilisateur entre 0)
+        // Cette boucle va constament demandez l'utilisateur pour une entrée et va par la suite lui présenter le nombre correspondant
+        // Le boucle utilise le fonction get pour recevoir l'entrée de l'utilisateur et le fonction convert pour convertir
+        // l'entrée en nombre.
         while (true) {
-            System.out.print("Entre une lettre: ");
+            System.out.print("Entrez une lettre, ou entrez 0 pour quitter le programme: ");
             String lettre = get();
             if (lettre == null){
-                System.out.println("Entrée invalide. Arrêt du programme");
                 System.exit(0);
             }
             else {
@@ -17,19 +30,28 @@ public class Travaux5 {
         }
     }
     private static String get() {
+        // Ce fonction retourne un charactère ou null (si l'entrée de l'utilisateur est 0). Si l'utilisateur met un
+        // entrée invalide. Le fonction demande pour une nouveau entrée
         Scanner input = new Scanner(System.in);
         String in = input.next();
-        char c = in.charAt(0);
-        String newC = Character.toString(c);
-        if(Character.isLetter(c)){
-            return newC.toUpperCase();
+        while (true) {
+            char c = in.charAt(0);
+            String newC = Character.toString(c);
+            if(Character.isLetter(c)){
+                return newC.toUpperCase();
+            }
+            else if (newC.equals("0")){
+                return null;
+            }
+            else{
+                System.out.print("Insérer un charactère valide: ");
+                in = input.next();
+            }
         }
-        else{
-            return null;
-        }
-
     }
     private static int convert(String lettre){
+        // Se fonction prend le charactère donné par le fonction get et avec l'aide d'une structure de selection switch
+        // il retourne le nombre correspondant qui est ensuite présenté à l'utilisateur
         switch (lettre){
             case "W" :
                 case "X" :
